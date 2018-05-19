2722ad50c7e533764081f412a9e03ff7bf3c3eb4
?????·è?? <shiruoxi2010@163.com>
2018-05-19 15:57:11 +0800

    Create Project

---
+++ src/cn/s3bit/droidauxline/CanvasView.java
@@ -0,0 +1,17 @@
+package cn.s3bit.droidauxline;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class CanvasView extends View {
+
+	public CanvasView(Context context) {
+		super(context);
+	}
+	
+	@Override
+	public void draw(Canvas canvas) {
+		
+	}
+}
---
+++ src/cn/s3bit/droidauxline/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.s3bit.droidauxline;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
