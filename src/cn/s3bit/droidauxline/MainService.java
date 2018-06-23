2a4f89d224fd7102f0f8879c7c1698bba5bf7d5d
北海若 <shiruoxi2010@163.com>
2018-06-23 21:09:48 +0800

    Lock Rotation

--- src/cn/s3bit/droidauxline/MainService.java
+++ src/cn/s3bit/droidauxline/MainService.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 import android.app.Application;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.util.DisplayMetrics;
+import android.view.Surface;
 import android.view.WindowManager;
 
 public class MainService extends Service {
@@ -27,6 +30,10 @@ public class MainService extends Service {
         params.height = WindowManager.LayoutParams.MATCH_PARENT;
         params.format = PixelFormat.TRANSPARENT;
 		WindowManager windowManager = (WindowManager) this.getSystemService(Application.WINDOW_SERVICE);
+		if (windowManager.getDefaultDisplay().getRotation() == Surface.ROTATION_0 || windowManager.getDefaultDisplay().getRotation() == Surface.ROTATION_180)
+			params.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+		else
+			params.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
         windowManager.addView(canvasView, params);
 		super.onCreate();
 	}
